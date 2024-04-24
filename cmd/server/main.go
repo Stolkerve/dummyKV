2f0001c11c732aef1e2f9f11191412f398e9020d
Sebastian Gonzalez <devsebasgr@gmail.com>
2024-04-24 17:15:06 -0400

    cli interface for the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"encoding/gob"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/Stolkerve/dummyKV/internal"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	gob.Register([]internal.Message{})
-	internal.Listen("0.0.0.0:8000")
+
+	var port, address string
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "port",
+				Value:   "8000",
+				Usage:   "Set the server port",
+				Aliases: []string{"p"},
+			},
+			&cli.StringFlag{
+				Name:    "address",
+				Value:   "0.0.0.0",
+				Usage:   "Set the server address",
+				Aliases: []string{"addr"},
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			port = ctx.String("port")
+			address = ctx.String("address")
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
+	internal.Listen(fmt.Sprintf("%s:%s", address, port))
 }
